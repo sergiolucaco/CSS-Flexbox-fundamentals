8f8be3d7c24f6b4aa3b1c6fb5d154d9975b3b618
Sergio Lucas <sergioluk@gmail.com>
2016-12-26 22:42:18 +0100

    adding jQuery and mediaqueries to the mobile project

--- flexbox_mobile_sample/js/script.js
+++ flexbox_mobile_sample/js/script.js
@@ -1,5 +1,12 @@
 $(document).ready(function() {
 
+	$("#magnify").click(function(){
+		$(this).hide(); // this === #magnify
+		// $("#search").show(); //no animation
+		$("#search").toggle("scale"); 
+		$("#search").css("display","block"); 
+
+	})
 
 });
 
